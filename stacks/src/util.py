92f4a574f69e652a971a6c96d91c7719471b6375
Hammy <sgoudham@gmail.com>
2021-08-28 22:33:13 +0100

    Include mypy for type checking

--- stacks/src/util.py
+++ stacks/src/util.py
@@ -1,11 +1,12 @@
+from troposphere import Template
 from troposphere.sqs import Queue
 
 
 class Util:
-    def __init__(self, template):
+    def __init__(self, template: Template) -> None:
         self.template = template
 
-    def create_queue(self, queue_name):
+    def create_queue(self, queue_name: str) -> Queue:
         return self.template.add_resource(
             Queue(
                 queue_name,
